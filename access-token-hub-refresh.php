291692b96a80994fd096839916a5356d8c968c1e
Mr-Lakshan <laksansingh235@gmail.com>
2025-01-17 15:17:12 +0530

    modified file and its variable

--- access-token-hub-refresh.php
+++ access-token-hub-refresh.php
@@ -1,14 +1,15 @@
 <?php
 
-$clientId = '';        
-$clientSecret = ''; 
-$refreshToken = ''; 
+
+$clientId = '<add client-id here >';        // add client-id here 
+$clientSecret = '<add client-secret here>'; // add client-secret here 
+$refreshToken = '<add refresh token here>'; // add refresh token here
 
 
 $url = 'https://api.hubapi.com/oauth/v1/token';
 
 
-$data = [
+$data1 = [
     'grant_type' => 'refresh_token',
     'refresh_token' => $refreshToken,
     'client_id' => $clientId,
@@ -21,7 +22,7 @@ $ch = curl_init();
 // Set cURL options
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data1));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/x-www-form-urlencoded',
@@ -36,7 +37,7 @@ if (curl_errno($ch)) {
 } else {
     
     $responseData = json_decode($response, true);
-    print_r($responseData);
+    // print_r($responseData);
 }
 
 
